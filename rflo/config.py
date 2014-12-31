54a7b166808a6fc1e3d1ff792a0367579c41080b
Thomas S Hatch <thatch45@gmail.com>
2014-12-31 11:00:39 -0700

    initial commit of config behavior

---
+++ rflo/config.py
@@ -0,0 +1,33 @@
+'''
+Behaviors to generate the config
+'''
+import ioflo.base.deeding
+import yaml
+import copy
+import io
+import socket
+
+DEFAULTS = {
+    'interface': '0.0.0.0',
+    'port': '7750',
+    }
+
+
+class RaftConfig(ioflo.base.deeding.Deed):
+    '''
+    '''
+    Ioinits = {'opts': '.etc.opts'}
+
+    def action(self):
+        '''
+        Read in the config
+        '''
+        self.opts.value = copy.deepcopy(DEFAULTS)
+        path = '/etc/rflo/main'
+        try:
+            with io.open(path, 'rb') as fp_:
+                self.opts.value.update(yaml.safe_load(fp_.read()))
+        except (IOError, OSError):
+            pass
+        if not self.opts.value.get('id'):
+            self.opts.value['id'] = socket.getfqdn()
