ae60acddce9f8d7699df1bbae4b1afeb9c9f7620
Thomas S Hatch <thatch45@gmail.com>
2014-12-31 10:49:33 -0700

    initial commit of rads behaviors

---
+++ rflo/roads.py
@@ -0,0 +1,64 @@
+'''
+The road stack behaviors
+'''
+
+import os
+from collections import deque
+
+import ioflo.base.deeding.Deed
+from raet import raeting, nacling
+from raet.road.stacking import RoadStack
+from raet.road.estating import RemoteEstate
+import raet.keeping
+
+class RaftRoadStackSetup(ioflo.base.deeding.Deed):
+    '''
+    Init the main road stack to use
+    '''
+    Ioinits = {
+            'inode': 'raft.',
+            'stack': 'road',
+            'txmsgs': {'ipath': 'txmsgs',
+                       'ival': deque()},
+            'rxmsgs': {'ipath': 'rxmsgs',
+                       'ival': deque()},
+            'opts': '.etc.opts',
+            }
+
+    def postinitio(self):
+        '''
+        Assign default stack values
+        '''
+        RoadStack.Bk = raeting.bodyKinds.msgpack
+        RoadStack.JoinentTimeout = 0.0
+
+    def action(self):
+        '''
+        Setup the roadstack, only cal this once with an enter
+        '''
+        kind = self.opts.value.get('kind', 'raft')
+        role = self.opts.value['id']
+        name = '{0}_{1}'.format(role, kind)
+        main = self.opts.value.get('raet_main', self.local.data.main)
+        mutable = self.opts.value.get('raet_mutable', self.local.data.mutable)
+        always = self.opts.value.get('open_mode', False)
+        mutable = mutable or always
+        uid = None
+        basedirpath = os.path.abspath(os.path.join(self.opts.value['cachedir'], 'raet'))
+        txMsgs = self.txmsgs.value
+        rxMsgs = self.rxmsgs.value
+        keep = raet.keeping.Keep(basedirpath)
+        ha = (self.opts.value['interface'], self.opts.value['port'])
+        self.stack.value = RoadStack(store=self.store,
+                                     keep=keep,
+                                     name=name,
+                                     uid=uid,
+                                     ha=ha,
+                                     role=role,
+                                     main=main,
+                                     kind=kind,
+                                     mutable=mutable,
+                                     txMsgs=txMsgs,
+                                     rxMsgs=rxMsgs,
+                                     period=3.0,
+                                     offset=0.5)
