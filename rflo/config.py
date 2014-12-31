2b77436bc7dde554e5e2f0b20ac2cb25166e5cd8
Thomas S Hatch <thatch45@gmail.com>
2014-12-31 13:32:59 -0700

    Add cli parsing

--- rflo/config.py
+++ rflo/config.py
@@ -1,11 +1,17 @@
 '''
 Behaviors to generate the config
 '''
+# Import ioflo
 import ioflo.base.deeding
-import yaml
+
+# Import python libs
 import copy
 import io
 import socket
+import argparse
+
+# Import third party libs
+import yaml
 
 DEFAULTS = {
     'interface': '0.0.0.0',
@@ -14,16 +20,39 @@ DEFAULTS = {
     }
 
 
+class RaftCli(ioflo.base.deeding.Deed):
+    '''
+    Parse the command line
+    '''
+    Ioinits = {'cli': '.etc.cli'}
+
+    def action(self):
+        '''
+        Parse the cli
+        '''
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+                '-p',
+                dest='port',
+                default='7750',
+                type=int,
+                )
+        args = parser.parse_args()
+        self.cli.value = dict(args)
+
 class RaftConfig(ioflo.base.deeding.Deed):
     '''
+    Read in the config file
     '''
-    Ioinits = {'opts': '.etc.opts'}
+    Ioinits = {'opts': '.etc.opts',
+               'cli': '.etc.cli'}
 
     def action(self):
         '''
         Read in the config
         '''
         self.opts.value = copy.deepcopy(DEFAULTS)
+        self.opts.update(self.cli.value)
         path = '/etc/rflo/main'
         try:
             with io.open(path, 'rb') as fp_:
