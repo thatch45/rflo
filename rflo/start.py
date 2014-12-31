4957b6685b5df1987727198ee3b2ceda0a9cfd95
Thomas S Hatch <thatch45@gmail.com>
2014-12-31 11:34:11 -0700

    Add manager classes

---
+++ rflo/start.py
@@ -0,0 +1,20 @@
+import ioflo.app.run.start
+import os
+
+
+class Manager(object):
+    '''
+    Manage the main ioflo process
+    '''
+    def __init__(self):
+        self.behaviors = ['rflo.config', 'rflo.roads']
+        self.floscript = os.path.join(os.path.dirname(__file__), 'raft.flo')
+
+    def start(self):
+        ioflo.app.run.start(
+                name='rflo',
+                period='0.01',
+                stamp=0.0,
+                filepath=self.floscript,
+                behaviors=self.behaviors,
+                )
