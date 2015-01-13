88e40ebc47115fb4bf501186b5701e0854dc5b19
Thomas S Hatch <thatch45@gmail.com>
2015-01-12 21:51:25 -0700

    make the router very simple at placing data in the work queues

--- rflo/router.py
+++ rflo/router.py
@@ -1,7 +1,10 @@
 '''
 The core message routing
 '''
+# Import python libs
+import collections
 
+# Import raet/ioflo libs
 import ioflo.base.deeding
 
 
@@ -10,6 +13,9 @@ class RaftRouter(ioflo.base.deeding.Deed):
             'opts': '.etc.opts',
             'txmsgs': '.raft.txmsgs',
             'rxmsgs': '.raft.rxmsgs',
+            'work': {
+                'ipath': '.raft.work',
+                'ival': collections.defaultdict(collections.deque)},
             }
 
     def action(self):
@@ -18,4 +24,20 @@ class RaftRouter(ioflo.base.deeding.Deed):
         '''
         # Just make sure it works for now
         while self.rxmsgs.value:
-            print(self.rxmsgs.value.popleft())
+            msg, sender = self.rxmsgs.value.popleft()
+            if 'share' in msg:
+                self.work.value[msg['share']].append((msg, sender))
+
+
+class RaftWorkClean(ioflo.base.deeding.Deed):
+    '''
+    Clean up the work queue of any invalid options, this is to prevent any
+    unhanddled data from piling up in memory. This empties the work dict
+    '''
+    Ioinits = {'work', '.raft.work'}
+
+    def action(self):
+        '''
+        Clear the work of rubbish
+        '''
+        self.work.value = collections.defaultdict(collections.deque)
